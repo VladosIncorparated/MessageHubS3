0a7e73a5ce04f16befb8df5cd46f15a2e63a777f
VladosIncorparated <podorvan03@mail.ru>
2025-02-01 16:20:02 +0900

    3

--- app.sh
+++ app.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-    sleep 10 &&
-    mc alias set myminio http://minio:9000 youraccesskey yoursecretkey &&
-    mc admin bucket cors set myminio/my-local-bucket /etc/mc/cors.json &&
-    mc admin policy set myminio/my-local-bucket /etc/mc/policy.json &&
-    ls
+sleep 10 &&
+mc alias set myminio http://minio:9000 youraccesskey yoursecretkey &&
+mc admin bucket cors set myminio/my-local-bucket /etc/mc/cors.json &&
+mc admin policy set myminio/my-local-bucket /etc/mc/policy.json &&
+ls
